080d4d7ef889101eb527f16ec3099ea1b6b4abb8
吳元皓 <hw@yuanhau.com>
2025-09-08 13:23:51 +0800

    this is not fun

--- code/hello-world.c
+++ code/hello-world.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
-    printf("Hello World");
+    printf("Hello World\n");
+    while (1) {
+        printf("Hello World \n");
+        usleep(300000);
+    }
     return 0;
 }
