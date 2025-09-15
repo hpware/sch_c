04e7bd5d92d7766c51c087d5e7825e222f7126d8
吳元皓 <hw@yuanhau.com>
2025-09-15 11:55:40 +0800

    i have no fucking clue want am i doing rn rn

---
+++ code/idkidkidk.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("我的學號是 51331102 \n");
+    printf("C 語言的世界, 您好啊！! \n");
+    return 0;
+}
---
+++ code/my-name.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("我的學號是 51331102 \n");
+    return 0;
+}
