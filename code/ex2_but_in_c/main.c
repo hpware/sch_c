c2c8bb7f0927d23f819289fe476b63103a11f441
Howard <hw@yuanhau.com>
2025-10-27 10:52:23 +0800

    fuck this shit

---
+++ code/ex2_but_in_c/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+float h,w, BMI;
+int main() {
+    printf("請輸入高度:");
+    scanf("%d", h);
+    printf("\n請輸入重量")
+    BMI = w/h/h;
+    printf("BMI = %f\n", BMI);
+    return 0;
+}
