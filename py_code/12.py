ba38d30c490e0df8cf3c7df3c5644470029d4188
Howard <hw@yuanhau.com>
2025-12-15 14:26:56 +0800

    iiii

--- py_code/12.py
+++ py_code/12.py
@@ -4,5 +4,6 @@ print("H=", H * 100, "cm, W=", W, "kg, BMI=", W / H / H, sep="")
 print("H=%dcm,W=%.1fkg,BMI=%.2f" % (H * 100, W, W / H / H))
 print(f"H={H * 100}cm,W={W}kg,BMI={W / H / H}")
 print(f"H={H * 100:.0f}cm,W={W:.1f}kg,BMI={W / H / H:.2f}")
+print("Test" * 3)
 # js
 # print(`H=${H*100}cm, W=${W}kg, BMI= ${W/H/H}`)
